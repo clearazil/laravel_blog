c34a50011f3806c8e99ba3a88098776e57a64f19
Derk <derkvanderheide@hotmail.com>
2024-09-18 15:44:59 +0200

    make image nullable

--- database/migrations/2024_09_18_123744_create_articles_table.php
+++ database/migrations/2024_09_18_123744_create_articles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->string('title');
             $table->text('content');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->boolean('is_premium');
             $table->timestamps();
         });
