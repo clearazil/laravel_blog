15531cadf0df6e03bbf6eeccc81425bef7d0a9e1
Derk <derkvanderheide@hotmail.com>
2024-10-09 14:54:40 +0200

    create factories and seeders

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->words(rand(1, 2), true),
+            'content' => fake()->paragraphs(rand(1, 4), true),
+            'is_premium' => fake()->boolean(50),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +18,28 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::factory()
+            ->count(10)
+            ->create();
+
+        $articles = Article::factory()
+            ->count(20)
+            ->recycle($users)
+            ->create();
+
+        Comment::factory()
+            ->count(40)
+            ->recycle($users)
+            ->recycle($articles)
+            ->create();
+
+        $categories = Category::factory()
+            ->count(10)
+            ->create();
+
+        // attach 3 to 5 random categories to each article
+        foreach ($articles as $article) {
+            $article->categories()->saveMany($categories->random(rand(3, 5)));
+        }
     }
 }
